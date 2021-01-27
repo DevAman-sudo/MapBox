e2c52ee97daa0823aec544b689abeb01e4e74f5b
DevAman-sudo <devaman.net@gmail.com>
2021-01-27 08:36:23 +0545

    setup mapbox

---
+++ js/index.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGV2YW1hbiIsImEiOiJja2tldGR4a28wMTI4MnBvNTZwNDZ4MmJlIn0.tNAqdVcfPOW_sBZd9Hk3Bw';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
