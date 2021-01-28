2ff7aae4b4512fdd1ff9005ea30ae41a04b07451
DevAman-sudo <devaman.net@gmail.com>
2021-01-28 09:04:26 +0545

    mapbox search map

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-node_modules
+// npm packages
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const chalk = require('chalk');
+const path = require('path');
+
+const staticPath = path.join(__dirname , '/public/');
+
+app.use(express.static(staticPath));
+
+app.get('/' , (req , res) => {
+    res.sendFile( path.join(staticPath , 'index.html'));
+});
+
+app.listen(port , (error) =>{
+    if (error) {
+        console.log(`Error Found => ${error}`);
+    } else {
+        console.log( chalk.red.bgBlue.bold(`server listening on port ${port}`));
+    }
+});
---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGV2YW1hbiIsImEiOiJja2tldGR4a28wMTI4MnBvNTZwNDZ4MmJlIn0.tNAqdVcfPOW_sBZd9Hk3Bw';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-79.4512, 43.6568],
+    zoom: 13
+});
+
+// search Map 
+var geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    marker: {
+        color: 'blue'
+    },
+    mapboxgl: mapboxgl
+});
+
+map.addControl(geocoder);
